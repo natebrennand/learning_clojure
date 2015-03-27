cfd4b54e40d423e4265bb39cbc967c2d4a3d7765
Nate Brennand <natebrennand@gmail.com>
2015-03-27 19:01:08 -0400

    starting section 4

--- brave_clojure/do-things.clj
+++ brave_clojure/do-things.clj
@@ -191,12 +191,106 @@
   (first things))
 (other-my-first ["oven" "bike" "waraxe"]) 
 
-
 (defn chooser
   [[first-choice second-choice & unimportant-choices]]
   (println (str "your 1st choice is: " first-choice))
   (println (str "your 2nd choice is: " second-choice))
   (println (str "we're ignoring the rest of your choices: " (clojure.string/join ", " first-choice))))
+
+
+;; destructuring maps
+(defn announce_treasure_location
+  [{lat :lat lng :lng}]
+  (println (str "latitude: " lat))
+  (println (str "longtitude: " lng)))
+(announce_treasure_location {:lat 23 :lng 30})
+
+(defn announce_treasure_location-2
+  [{:keys [lat lng]}]
+  (println (str "latitude: " lat))
+  (println (str "longtitude: " lng)))
+(announce_treasure_location-2 {:lat 23 :lng 30})
+
+(defn announce_treasure_location-3
+  [{:keys [lat lng] :as treasure-location}]
+  (println (str "latitude: " lat))
+  (println (str "longtitude: " lng))
+  (println (str "treasure at: " treasure-location)))
+(announce_treasure_location-3 {:lat 23 :lng 30})
+
+
+;; function body exploration
+(defn illustrative-function
+  []
+  (+ 1 304)
+  30
+  "joe")
+(illustrative-function)
+
+(defn number-comment
+  [x]
+  (if (> x 6)
+    "ob my gosh, what a large number"
+    "that's an OK number I guess"))
+(number-comment 4)
+(number-comment 8)
+
+
+
+;; there are many ways to declare functions
+(map (fn [name] (str "Hi, " name))
+     ["darth vader" "mr magoo"])
+((fn [x] (* 3)) 8)
+
+(def my-special-multiplier (fn [x] (* x 3)))
+(my-special-multiplier 12)
+
+
+;; compact lambdas
+#(* % 3)
+(#(* % 3) 8)
+(map #(str "hi, " %)
+     ["darth vader" "mr magoo"])
+
+(def cube #(* % % %))
+(cube 3)
+
+(def multiply #(* %1 %2))
+(multiply 2 3)
+
+
+(#(identity %&) 1 "blab" :hi)
+
+
+
+;; returning functions
+(defn inc-maker
+  "creates a custom incrementer"
+  [inc-by]
+  #(+ inc-by %))
+(def inc3 (inc-maker 3))
+(inc3 7)
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 4.1 Pulling it all together
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
