c6bc70508b1c9b1a0a323bf17e1fb9e85a5729b0
Nate Brennand <natebrennand@gmail.com>
2015-04-03 01:16:46 -0400

    add problems 7 & 8

---
+++ euler/7.clj
@@ -0,0 +1,27 @@
+
+; Problem 7
+;   10001st prime
+; By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+; What is the 10 001st prime number?
+
+
+(defn divisible?
+  [n nums]
+  (some #(= 0 (mod n %)) nums))
+
+
+(defn primes
+  [n]
+  (loop [i 6
+         p [2 3 5]]
+    (if (= n (.length p))
+      (- i 1)
+      (recur (+ i 1) (if (divisible? i p)
+                       p
+                       (conj p i))))))
+
+(primes 10001)
+; 104743
+
+
+
